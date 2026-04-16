2212265cd843a50ef321786d82c58226efcb654a
Getayalew <getedu7@gmail.com>
2026-04-16 18:31:39 +0300

    final touches database.php

--- database.php
+++ database.php
@@ -1,5 +1,4 @@
 <?php
-// config.php - Database configuration and connection
 session_start();
 
 $servername = "localhost";
@@ -8,11 +7,8 @@ $password = "NewstrongPassword";
 $dbname = "LoginandRegistration";
 
 try {
-    // Create PDO connection
     $pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
-    // Set PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // Set default fetch mode to associative array
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch(PDOException $e) {
     die("Connection failed: " . $e->getMessage());
